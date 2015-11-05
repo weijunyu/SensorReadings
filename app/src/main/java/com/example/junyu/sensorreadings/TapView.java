55698f71ff82519425a1dd0518f6dbb20497a4d5
Junyu Wei <junyuwei@gmail.com>
2015-11-05 03:36:25 +0000

    Changed minSdk to 15, changed app bar color, added TapView.java
    App bar color: content_main.xml.
    TapView is planned to be a custom view for drawing tapping locations

---
+++ app/src/main/java/com/example/junyu/sensorreadings/TapView.java
@@ -0,0 +1,11 @@
+package com.example.junyu.sensorreadings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TapView extends View {
+    public TapView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+}
