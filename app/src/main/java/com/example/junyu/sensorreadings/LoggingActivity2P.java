f64c9795d69a76fd3f1241cdd2c1f055d96ab290
Junyu Wei <junyuwei@gmail.com>
2015-11-28 08:44:50 +0000

    Refactored LoggingActivity names because GitHub isn't case sensitive

--- app/src/main/java/com/example/junyu/sensorreadings/LoggingActivity2P.java
+++ app/src/main/java/com/example/junyu/sensorreadings/LoggingActivity2P.java
@@ -11,8 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
--- app/src/main/java/com/example/junyu/sensorreadings/LoggingActivity5P.java
+++ app/src/main/java/com/example/junyu/sensorreadings/LoggingActivity5P.java
@@ -11,8 +11,6 @@ import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
